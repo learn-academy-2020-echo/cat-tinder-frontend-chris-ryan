d84f06c3906a68f7cf6f070780ba6310d7c7a148
chrisolorobin <camorton88@gmail.com>
2021-01-07 11:55:23 -0800

    Ensuring Read-Func is most recent code

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Card, CardTitle, Col } from 'reactstrap'
+
 
 class CatIndex extends Component {
     render(){
+        console.log(this.props.cats)
         return(
             <>
                 <h1>Here are all the cats!</h1>
+                
+
             </>
         )
     }
